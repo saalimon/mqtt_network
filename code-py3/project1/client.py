3d110e5965a14ab8de034d4849160ef8f22440d7
saalimon <niti08@gmail.com>
2019-02-19 18:54:40 +0700

    chang variable

--- code-py3/project1/client.py
+++ code-py3/project1/client.py
@@ -11,33 +11,31 @@ MAX_BUF = 2048
 port = 50000 
 addr = (host,port)
 s = socket(AF_INET, SOCK_DGRAM,IPPROTO_UDP)
+s.bind((MCAST_GRP,MAX_BUF))
 topic = dict()
 def recvMsg():
-  try:
-    recvpack, payload = s.recvfrom(1024)
-  except error:
-    recvpack = None
-  if recvpack is not None:
-    t =json.loads(recvpack.decode('utf-8'))
-    for k,v in t.items():
-      topic[k] = v
-threading.Thread(target = recvMsg, args = ()).start()
+  recvpack, payload = s.recvfrom(1024)
+  t =json.loads(recvpack.decode('utf-8'))
+  print(t)
+  # for k,v in t.items():
+  #   topic[k] = v
 while True:
   txtout = sys.stdin.readline().strip()
   command = txtout.split()
   if len(command) > 1 & len(command) <=3 :
     if len(command) == 2:
-      if command[0] == 'subscribe':
+      if command[0] == 's':
         topic[command[1]] = ''
       else:
         print("Invalid command")
     else :
-      if command[0] == 'publish':
+      if command[0] == 'p':
         topic[command[1]] = command[2]
     data_string = json.dumps(command)
     s.sendto(data_string.encode('utf-8'),addr)  
-  if txtout == 'quit':
+  if txtout == 'q':
     break
-  if txtout == 'list':
+  if txtout == 'l':
     print(topic)
+  threading.Thread(target = recvMsg, args = ()).start()
 s.close()   
--- code-py3/project1/server.py
+++ code-py3/project1/server.py
@@ -17,10 +17,8 @@ topic = dict()
 list_of_sub = list()
 print ('Broker started ...')
 s.bind(addr) 
-def sendMsg():
-  for i in list_of_sub:
-    s.sendto(data_string.encode('utf-8'), i)
-threading.Thread(target = sendMsg, args = ()).start()
+def sendMsg(address):
+    s.sendto(data_string.encode('utf-8'), address)
 while True:
   txtin,addr = s.recvfrom(MAX_BUF)
   data_loaded = json.loads(txtin.decode('utf-8'))
@@ -28,21 +26,19 @@ while True:
   if txtin == b'quit':          
      print('Terminate server ...')
      break
-  if data_loaded[0]=='subscribe':
+  if data_loaded[0]=='s':
     if data_loaded[1] not in topic:
       topic[data_loaded[1]] = ''        #data_loaded[1] is topic_name
       # client_port = json.dumps({'port':addr[1]})
       # s.sendto(client_port.encode('utf-8'),addr)
-  elif data_loaded[0]=='publish':
+  elif data_loaded[0]=='s':
     if data_loaded[1] in topic:
       topic[data_loaded[1]] = data_loaded[2]  #data_loaded[1] is topic_name  
   data_string = json.dumps(topic)
   if addr not in list_of_sub:
     list_of_sub.append(addr)
-    # print("OK")
   for i in list_of_sub:
-    s.sendto(data_string.encode('utf-8'), i)
+    threading.Thread(target = sendMsg, args = (i,)).start()
   print(topic)
-  # s.sendto("hello".encode('utf-8'), (host,port))
 
 s.close()      
